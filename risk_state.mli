dba7d838c2819fe6ba3350bc8fccd72c2ee4302a
Sam Hu <sh924@cornell.edu>
2017-11-14 22:20:04 -0500

    updated state

--- risk_state.mli
+++ risk_state.mli
@@ -3,17 +3,30 @@
  * belonging to each player, the total number of turns etc. *)
 type state
 
-(* [trade_in cmd st] is the state that results after processing command cmd *)
+(* [init_state bd p_list st] is the initial state of the game board [bd] with
+ * the players in [p_list] *)
+val init_state : Board.board -> Board.player list -> state
+
+(* [trade_in cmd st] is the state that results after processing command cmd
+ * [trade_in] allows the player to redeem cards for additional troops *)
 val trade_in : Command.command -> state -> state
 
-(* [reinforce cmd st] is the state that results after processing command cmd *)
+(* [reinforce cmd st] is the state that results after processing command cmd
+ * [reinforce] adds troops to a country of a player's choice, so long as the
+ * owns that country *)
 val reinforce : Command.command -> state -> state
 
-(* [attack cmd st] is the state that results after processing command cmd *)
+(* [attack cmd st] is the state that results after processing command cmd
+ * [attack] allows the player to select a country to attack an opponent's
+ * country using a randogm dice-rolling system *)
 val attack : Command.command -> state -> state
 
-(* [fortify cmd st] is the state that results after processing command cmd *)
+(* [fortify cmd st] is the state that results after processing command cmd
+ * [fortify] allows the player to move troops from one country to another
+ * country reachable from the first country through adjacent bordering countries *)
 val fortify : Command.command -> state -> state
 
-(* [pass cmd st] is the state that results after processing command cmd *)
+(* [pass cmd st] is the state that results after processing command cmd
+ * [pass] allows the user to do nothing during his or her turn, or to signal the
+ * end of a turn *)
 val pass : Command.commmand -> state -> state
