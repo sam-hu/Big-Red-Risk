e0754ab34291071e38bf82e74d6a1a42426598ef
longliveimani <chilongani.imani@gmail.com>
2017-11-14 22:45:42 -0500

    command docs

--- command.mli
+++ command.mli
@@ -1,37 +1,39 @@
+(*[point] is a point on the users screen *)
+type point
+
 (* [command] represents a command input by a player. *)
-(* You may redefine [command] to be synonymous with whatever
- * type you wish, but its name must not be changed.
- * It is okay to expose this type rather than make it abstract,
- * because it is not the representation type of a data structure. *)
-type command = {
-  com: string;
-  word: string;
-}
+type command
 
-(********************************************************
- * DO NOT CHANGE THIS CODE
- * It is part of the interface the course staff will
- * use to test your submission.
- *)
 
-(* [parse str] is the command that represents player input [str].
- * requires: [str] is one of the commands forms described in the
- *   assignment writeup. *)
-val parse : string -> command
 
-(* END DO NOT CHANGE
- ********************************************************)
-(* You are free to add more code below *)
+(* [parse point1 point2 str1] is the command that represents player input
+   [point1] [point2] [str1]
+ * requires: [point1] and [point2] are points on the gui, str1 is a string
+   describing the type of command*)
+val parse : point -> point -> string -> command
 
-(* [makeGoCommand str] creates a go command with c.word of [str].
- * requires: [str] is a string. *)
-val makeGoCommand: string -> command
 
-(* [makeCommand str1 str2] creates a command with c.com of [str1] and
- * c.word of [str2].
- * requires: [str1] and [str2] are strings. *)
-val makeCommand: string -> string -> command
+(* [makeAttackCommand point1 point2] creates a attack command
+ * requires: [point1] and [point2] are points. *)
+val makeAttackCommand: point -> point -> command
 
-(* [falseCommand] creates a false command with c.com of "" and c.word of "".
- * requires: None. *)
+(* [makeReinforceCommand point1] creates a reinforce command
+ * requires: [point1] is a point. *)
+val makeReinforceCommand: point -> command
+
+(* [makeFortifyCommand point1 point2] creates a reinforce command
+ * requires: [point1] and [point2] are points. *)
+val makeFortifyCommand: point -> point -> command
+
+(* [makeTradeInCommand] creates a trade-in command  *)
+val makeTradeInCommand:  command
+
+(* [makePassCommand str] creates a pass command  *)
+val makePassCommand: command
+
+(* [makeCommand point1 point2 str1] creates one of the 5 valid commands or a false command
+ * requires: [point1] and [point2] are point, and [str1] is a string *)
+val makeCommand: point -> point -> string -> command
+
+(* [falseCommand] creates a false command  *)
 val falseCommand: command
