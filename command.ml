954958e9cd78876bc9dd626de4a9e8c780d5631a
Sam Hu <sh924@cornell.edu>
2017-11-30 23:28:57 -0500

    added basic command checking

--- command.ml
+++ command.ml
@@ -1,17 +1,77 @@
 open Board
+open State
 
-type attack_command = {
-  from_noun: string;
-  to_noun: string;
-}
+type attack_command = FalseAttack | Attack of (string*string)
 
-type fortify_command = {
-  from_noun : string;
-  to_noun : string;
-}
+type fortify_command = FalseFortify | Fortify of string
 
-type reinforce_command = string
+type reinforce_command = FalseReinforce | Reinforce of string
 
-type pass_command = string
+type pass_command = unit
 
-type trade_command = Same of card | Different
+(* type card = Circle|Triangle|Square *)
+
+type trade_command = NoTrade | Same of card | Different
+
+let rec attack_helper attacker defender player countries good_attacker good_defender =
+  match countries with
+  | [] -> if (good_attacker && good_defender) then Attack (attacker, defender) else FalseAttack
+  | (c, p, i) :: t -> if (c = attacker && p = player) then attack_helper attacker defender player t true good_defender
+    else if (c = defender && p <> player) then attack_helper attacker defender player t good_attacker true
+    else attack_helper attacker defender player t good_attacker good_defender
+
+let make_attack_command attacker defender st =
+  let active_player = st.player_turn.player_id in
+  let countries = st.occupied_countries in
+  attack_helper attacker defender active_player countries false false
+
+let rec fortify_helper country player country_list =
+  match country_list with
+  | [] -> FalseFortify
+  | (c, p, i)::t -> if (c = country && p = player) then Fortify (country)
+    else fortify_helper country player t
+
+let make_fortify_command from_country st =
+  let active_player = st.player_turn.player_id in
+  let countries = st.occupied_countries in
+  fortify_helper from_country active_player countries
+
+let rec reinforce_helper country player country_list =
+  match country_list with
+  | [] -> FalseReinforce
+  | (c, p, i)::t -> if (c = country && p = player) then Reinforce (country)
+    else reinforce_helper country player t
+
+let make_reinforce_command to_country st =
+  let active_player = st.player_turn.player_id in
+  let countries = st.occupied_countries in
+  reinforce_helper to_country active_player countries
+
+let make_pass_command = ()
+
+let rec three_same card_list circles triangles squares =
+  if (circles = 3) then (true, Circle)
+  else if (triangles = 3) then (true,Triangle)
+  else if (squares = 3) then (true, Square)
+  else
+    (match card_list with
+     | [] -> (false, Circle)
+     | Circle::t -> three_same t (circles+1) triangles squares
+     | Triangle::t -> three_same t circles (triangles+1) squares
+     | Square::t -> three_same t circles triangles (squares+1))
+
+let rec three_diff card_list circles triangles squares =
+  if (circles > 0 && triangles > 0 && squares > 0) then true else
+    (match card_list with
+     | [] -> false
+     | Circle::t -> three_diff t (circles+1) triangles squares
+     | Triangle::t -> three_diff t circles (triangles+1) squares
+     | Square::t -> three_diff t circles triangles (squares+1))
+
+let make_trade_command st =
+  let cards = st.player_turn.cards in
+  let same = three_same cards 0 0 0 in
+  let diff = three_diff cards 0 0 0 in
+  if (fst same) then Same (snd same)
+  else if (diff) then Different
+  else NoTrade
--- command.mli
+++ command.mli
@@ -2,15 +2,15 @@
 (* type point *)
 
 (* [command] represents a command input by a player. *)
-type attack_command
+type attack_command = (string*string)
 
-type reinforce_command
+type reinforce_command = string
 
-type fortify_command
+type fortify_command = string
 
 type trade_command
 
-type pass_command
+type pass_command = unit
 
 (* [parse point1 point2 str1] is the command that represents player input
    [point1] [point2] [str1]
@@ -21,25 +21,25 @@ type pass_command
 
 (* [makeAttackCommand point1 point2] creates a attack command
  * requires: [point1] and [point2] are points. *)
-(* val makeAttackCommand: point -> point -> attack_command *)
+(* val make_attack_command: point -> point -> attack_command *)
 
 (* [makeReinforceCommand point1] creates a reinforce command
  * requires: [point1] is a point. *)
-(* val makeReinforceCommand: point -> reinforce_command *)
+(* val make_reinforce_command: point -> reinforce_command *)
 
 (* [makeFortifyCommand point1 point2] creates a reinforce command
  * requires: [point1] and [point2] are points. *)
-(* val makeFortifyCommand: point -> point -> fortify_command *)
+(* val make_fortify_command: point -> point -> fortify_command *)
 
 (* [makeTradeInCommand] creates a trade-in command  *)
-(* val makeTradeInCommand:  trade_command *)
+(* val make_trade_command:  trade_command *)
 
 (* [makePassCommand str] creates a pass command  *)
-(* val makePassCommand: pass_command *)
+(* val make_pass_command: pass_command *)
 
 (* [makeCommand point1 point2 str1] creates one of the 5 valid commands or a false command
  * requires: [point1] and [point2] are point, and [str1] is a string *)
-(* val makeCommand: point -> point -> string -> command *)
+(* val make_command: point -> point -> string -> command *)
 
 (* [falseCommand] creates a false command  *)
-(* val falseCommand: command *)
+(* val false_command: command *)
